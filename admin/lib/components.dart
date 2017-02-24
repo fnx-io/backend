de95b895afd333351a25e0b5de726f979d5c5689
Tomucha <tomucha@gmail.com>
2017-02-24 12:38:20 +0100

    Image picker component for form. Responsive menu.

--- admin/lib/components.dart
+++ admin/lib/components.dart
@@ -1,3 +1,4 @@
+import 'package:admin/components/fnx_image.dart';
 import 'package:admin/components/login_box.dart';
 import 'package:admin/components/gallery_picker/fnx_gallery_picker.dart';
 import 'package:admin/components/rest_listing_monitor.dart';
@@ -5,11 +6,13 @@ import 'package:admin/components/rest_listing_monitor.dart';
 export 'components/login_box.dart';
 export 'components/gallery_picker/fnx_gallery_picker.dart';
 export 'components/rest_listing_monitor.dart';
+export 'components/fnx_image.dart';
 
 const CUSTOM_COMPONENTS = const [
   LoginBox,
   FnxGalleryPicker,
-  RestListingMonitor
+  RestListingMonitor,
+  FnxImage
 ];
 
 const CUSTOM_PIPES = const [
