7ee8c3cfd3a1c9254d1b7ae388f6750dfa9724f1
Tomucha <tomucha@gmail.com>
2017-02-26 13:47:23 +0100

    Forgot to add directory :-)

---
+++ admin/lib/cms/screen_cms_listing.dart
@@ -0,0 +1,27 @@
+import 'package:admin/cms/module_cms.dart';
+import 'package:admin/rest_listing_factory.dart';
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+import 'package:fnx_rest/fnx_rest.dart';
+
+///
+/// ScreenNewsList
+///
+@Component(
+  selector: 'screen-cms-listing',
+  templateUrl: 'screen_cms_listing.html'
+)
+class ScreenCmsListing {
+
+  RestClient rest;
+  RestListing listing;
+  ModuleCms moduleCms;
+
+  String get type => moduleCms.type;
+
+  ScreenCmsListing(RestClient rootRest, this.moduleCms) {
+    rest = rootRest.child("/v1/cms/articles?type=${type}");
+    listing = RestListingFactory.withPaging(rest);
+  }
+  
+}
