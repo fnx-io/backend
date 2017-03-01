9c9d2af4ed1d40f29c7c516080b7141938648a22
Vladimir Kroupa <vladimir.kroupa@semantacorp.com>
2017-03-01 14:53:28 +0100

    extracted log listing component

--- admin/lib/components.dart
+++ admin/lib/components.dart
@@ -1,3 +1,4 @@
+import 'package:admin/components/log_listing_modal.dart';
 import 'package:admin/components/fnx_image.dart';
 import 'package:admin/components/login_box.dart';
 import 'package:admin/components/gallery_picker/fnx_gallery_picker.dart';
@@ -7,12 +8,14 @@ export 'components/login_box.dart';
 export 'components/gallery_picker/fnx_gallery_picker.dart';
 export 'components/rest_listing_monitor.dart';
 export 'components/fnx_image.dart';
+export 'components/log_listing_modal.dart';
 
 const CUSTOM_COMPONENTS = const [
   LoginBox,
   FnxGalleryPicker,
   RestListingMonitor,
-  FnxImage
+  FnxImage,
+  LogListingModal
 ];
 
 const CUSTOM_PIPES = const [
